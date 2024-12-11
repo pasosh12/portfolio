09eb5237cc824824328f4ba9a5860173605e95df
Daniil <daniil.shishaev@gmail.com>
2024-12-11 23:45:55 +0300

    1-02: container, globalStyles, theme added and Icons refactor

--- src/Layout/Header/Header.tsx
+++ src/Layout/Header/Header.tsx
@@ -3,54 +3,55 @@ import styled from "styled-components";
 import logo from "../../assets/logo.svg";
 import SvgIcon from "../../Components/SvgIcon/SvgIcon";
 import {FlexWrapper} from "../../Components/FlexWrapper";
+import {Container} from "../../Components/Container";
 
 
 const Header = () => {
     return (
-        <StyledHeader>
-            <img src={logo} alt={""}></img>
-            <FlexWrapper  gap="51px" alignItems="center">
-            <nav>
-                <StyledUl>
-                    <StyledLi>
-                        <a href="">Home</a>
-                    </StyledLi>
-                    <StyledLi>
-                        <a href="">About</a>
-                    </StyledLi>
-                    <StyledLi>
-                        <a href="">Tech Task</a>
-                    </StyledLi>
-                    <StyledLi>
-                        <a href="">Projects</a>
-                    </StyledLi>
-                    <StyledLi>
-                        <a href="">Contacts</a>
-                    </StyledLi>
-                </StyledUl>
-            </nav>
+        <Container>
+            <FlexWrapper justify="space-between" alignItems={"center"}>
+                <img src={logo} alt={""}></img>
+                <FlexWrapper gap="51px" alignItems="center">
+                    <nav>
+                        <StyledUl>
+                            <StyledLi>
+                                <a href="">Home</a>
+                            </StyledLi>
+                            <StyledLi>
+                                <a href="">About</a>
+                            </StyledLi>
+                            <StyledLi>
+                                <a href="">Tech Task</a>
+                            </StyledLi>
+                            <StyledLi>
+                                <a href="">Projects</a>
+                            </StyledLi>
+                            <StyledLi>
+                                <a href="">Contacts</a>
+                            </StyledLi>
+                        </StyledUl>
+                    </nav>
 
-            <FlexWrapper gap="20px" alignItems="center">
-                <a href={"#"}>
-                    <SvgIcon  iconsHref={socialLogos} iconId={"github_cat"} size={'30px'} viewBox={"0 0 30 30"}/>
-                </a>
-                <a href={"#"}>
-                    <SvgIcon iconsHref={socialLogos} iconId={"twitter"} size={"31px"} viewBox={"0 0 30 30"} />
-                </a>
-                <a href={"#"}>
-                    <SvgIcon iconsHref={socialLogos} iconId={"linkedin"} size={"31px"} viewBox={"0 0 30 30"}/>
-                </a>
+                    <FlexWrapper gap="20px" alignItems="center">
+                        <a href={"#"}>
+                            <SvgIcon iconsHref={socialLogos} iconId={"github_cat"} size={'30px'}
+                                     viewBox={"0 0 30 30"}/>
+                        </a>
+                        <a href={"#"}>
+                            <SvgIcon iconsHref={socialLogos} iconId={"twitter"} size={"31px"}
+                                     viewBox={"0 0 30 30"}/>
+                        </a>
+                        <a href={"#"}>
+                            <SvgIcon iconsHref={socialLogos} iconId={"linkedin"} size={"31px"}
+                                     viewBox={"0 0 30 30"}/>
+                        </a>
+                    </FlexWrapper>
+                </FlexWrapper>
             </FlexWrapper>
-            </FlexWrapper>
-        </StyledHeader>
+        </Container>
     )
 };
-const StyledHeader = styled.header`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-`
+
 const StyledLi = styled.li`
     color: #666666;
     font-weight: 500;
@@ -64,7 +65,6 @@ const StyledLi = styled.li`
 `
 const StyledUl = styled.ul`
     display: flex;
-    alig-items: flex-end;
     gap: 30px;
     list-style: none;
     align-items: center;
--- src/Styles/Global.styled.ts
+++ src/Styles/Global.styled.ts
@@ -1,4 +1,5 @@
 import {createGlobalStyle} from "styled-components";
+import {theme} from "./Theme";
 
 export const GlobalStyle = createGlobalStyle`
     *,
@@ -9,18 +10,23 @@ export const GlobalStyle = createGlobalStyle`
         box-sizing: border-box;
     }
     body {
-        margin: 0;
+        //margin: 0;
         font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
         sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+         //color: ${theme.colors.font};
+        display: flex;
+        flex-direction: column;
+        gap: 200px;
     }
     a{
         text-decoration: none;
     }
     ul{
         list-style: none;
+        padding: 0;
     }
     button{
         background-color: unset;
