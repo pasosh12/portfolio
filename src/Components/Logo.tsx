902f546c8c5db01d500e4f0e6bb292d3ffcddda4
Daniil <daniil.shishaev@gmail.com>
2024-12-18 23:51:15 +0300

    logo component created and removed unused imports from header

---
+++ src/Components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from "../assets/logo.svg";
+
+export const Logo = () => {
+    return (
+        <a href={"#"}>
+            <img src={logo} alt={"logo"}/>
+        </a>
+    );
+};
+
